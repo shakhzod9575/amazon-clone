6e7e0201980b8c2951e041826fcd998c632bbf1f
Shakhzod Elturaev <shahzodelturayevepam@gmail.com>
2025-07-05 12:59:53 +0500

    Created automated tests for formatCurrency

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,6 +1,6 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
-import { formatCurrency } from "../utils/money.js";
+import formatCurrency from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"
 import { renderPaymentSummary } from "./paymentSummary.js";
---
+++ tests/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if (formatCurrency(2095) === '20.95') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+if (formatCurrency(0) === '0.00') {
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Passed');
+} else {
+    console.log('Failed')
+}
