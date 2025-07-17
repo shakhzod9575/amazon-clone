8c9935e5c4db476bd6c3462481776a8b15d7bda6
Shakhzod Elturaev <shahzodelturayevepam@gmail.com>
2025-07-17 11:36:02 +0500

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,28 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     })
+// });
